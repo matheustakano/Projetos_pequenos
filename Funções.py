d7e67180b24e971c3e9e687072d38cfbb2b786ff
Tukano <71953842+Alborgheth@users.noreply.github.com>
2021-08-02 12:10:58 -0300

    Add files via upload

---
+++ Funções.py
@@ -0,0 +1,24 @@
+def numero_binomial():
+
+    n = int(input("Determine o número de n: "))
+    i = 0
+    f = 1
+    while i <= n and i != n:
+        f = f * 1
+        i = i + 1
+        f = i * f
+    print(f)      
+
+    k = int(input("Determine o número de k: "))
+    a = 0
+    b = 1
+    while a <= k and a != k:
+        b = b * 1
+        a = a + 1
+        b = a * b
+    print(b)
+
+    print("O resultado da divisão com o denominador:", f // b * (f - b))
+
+    
+

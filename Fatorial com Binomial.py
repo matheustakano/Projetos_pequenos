3165b7cffc4e7cb3894d32f42bb583ac299535e9
Matheus <71953842+Alborgheth@users.noreply.github.com>
2021-08-05 16:15:06 -0300

    Update Fatorial com Binomial.py

--- Fatorial com Binomial.py
+++ Fatorial com Binomial.py
@@ -1,24 +1,10 @@
-def numero_binomial():
+def fatorial(n):
+    fat = 1
+    while (n > 1):
+        fat = fat * n
+        n = n - 1
+    return fat
 
-    n = int(input("Determine o número de n: "))
-    i = 0
-    f = 1
-    while i <= n and i != n:
-        f = f * 1
-        i = i + 1
-        f = i * f
-    print(f)      
-
-    k = int(input("Determine o número de k: "))
-    a = 0
-    b = 1
-    while a <= k and a != k:
-        b = b * 1
-        a = a + 1
-        b = a * b
-    print(b)
-
-    print("O resultado é:", f // b * (f - b))
-
-    
+def numero_binomial(n, k):
+    return fatorial(n) // (fatorial(k) * fatorial(n - k))
 
